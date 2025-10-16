fae6501065ce01798494cd4ee7686976ebdd059c
ulugbek869 <godzilla019472@gmail.com>
2025-10-16 16:16:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+
+function Btn() {
+   let a = prompt("Qiyqiriq setdan qancha zakaz brmoqchisiz")
+   alert(`${a} ta Qiyqiriq set narxi ${a*45000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+
+function Btn1() {
+   let a = prompt("Snek setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Snek set narxi ${a*79000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn2() {
+   let a = prompt("Klassik setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Klassik set narxi ${a*34000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn3() {
+   let a = prompt("Longer setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Longer set narxi ${a*45000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn4() {
+   let a = prompt("Big setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Big set narxi ${a*62000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn5() {
+   let a = prompt("Lester setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Lester set narxi ${a*53000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn6() {
+   let a = prompt("School setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta School set narxi ${a*36000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn7() {
+   let a = prompt("Katta Combo setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Katta Combo set narxi ${a*20000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn8() {
+   let a = prompt("Dostlar 1x setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Dostlar 1x set narxi ${a*43000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn9() {
+   let a = prompt("Dostlar 2x setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Dostlar 2x set narxi ${a*79000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn10() {
+   let a = prompt("Dostlar 4x setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Dostlar 4x set narxi ${a*149000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
+function Btn11() {
+   let a = prompt("Dostlar Achhiq setdan qancha zakaz bermoqchisiz")
+   alert(`${a} ta Dostlar Achchiq set narxi ${a*43000}som`)
+   let b = confirm("zkazni qabul qilaylikmi??")
+   switch (b) {
+       case true:
+           alert("zakazingiz qabul qilindi")
+           break;
+       case false:
+           alert("zakaz bekor qilindi")
+           break;
+       default:
+           break;
+   }
+}
